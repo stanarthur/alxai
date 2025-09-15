7bd09c1606e7b484e0a741790970d88857e727d0
Stanhope Arthur <stanhopearthur@my.uopeople.edu>
2025-09-15 12:26:53 +0100

    feature added; prompt added  in index.ts

--- module_1/task_3/my-agent/index.ts
+++ module_1/task_3/my-agent/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";   // Import the google module from the ai-sdk package
+
+// Specify the model to use for generating text and a prompt
+const { text } = await generateText({
+  model: google("models/gemini-2.5-flash"),   
+  prompt: "What is an AI agent?", 
+});
+
+console.log(text);
